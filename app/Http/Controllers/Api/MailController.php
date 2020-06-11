94b90f111789932878c62c313fcbc1371c6d9b17
lulmairine <alex02.cailler@gmail.com>
2020-06-11 21:09:53 +0200

    mail

--- app/Http/Controllers/Api/MailController.php
+++ app/Http/Controllers/Api/MailController.php
@@ -3,19 +3,18 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Mail\MailContact;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 
 class MailController extends Controller
 {
-    public function receiveFrom() {
-        $to_name = 'ALexandre';
-        $to_email = 'alex02.cailler@gmail.com';
-        $data = array('name'=>'Ogbonna Vitalis(sender_name)', 'body' => 'A test mail');
-        Mail::send('emails.mail', $data, function($message) use ($to_name, $to_email) {
-            $message->to($to_email, $to_name)
-                ->subject('Laravel Test Mail');
-            $message->from('honyakuca@gmail.com','Test Mail');
-        });
+    public function receiveFrom(Request $request) {
+        Mail::to('alex02.cailler@gmail.com')->send(new MailContact($request));
+
+        return response([
+            'message' => 'Email envoyé avec succès !',
+            'status' => 200
+        ]);
     }
 }
